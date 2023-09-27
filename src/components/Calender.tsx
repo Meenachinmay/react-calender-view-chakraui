b2e938d1aff83c03dbf59fc4fb53c15941a6bec7
meenachinmay <chinmayanand896@gmail.com>
2023-09-28 08:09:29 +0900

    Initial commit, project started for adding calender-view to OEP

---
+++ src/components/Calender.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Flex, Text } from '@chakra-ui/react'
+
+const Calender:React.FC = () => {
+  return (
+    <Flex>
+    <Text>Hello world from calender</Text>
+    </Flex>
+  )
+}
+
+export default Calender;
